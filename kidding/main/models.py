9cf6c13d331015647348dcb21d2c07c23f36e3c6
aqwua0 <mbmeetlater@gmail.com>
2023-10-01 18:37:02 +0300

    main

--- kidding/main/models.py
+++ kidding/main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class shoes(models.Model):
     title = models.CharField('Название пары', max_length=255)
     content = models.TextField('Цена', blank=True)
     content1 = models.TextField('Артикул', blank=6)
+
+    def __str__(self):
+            return self.title
 
 class order(models.Model):
     title1 = models.CharField('ФИО', max_length=100)
--- kidding/main/urls.py
+++ kidding/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('page', views.index),
-    path('about', views.about),
-    path('login', views.login),
-    path('', views.regist),
+    path('page', views.index, name='page'),
+    path('about', views.about, name='about'),
+    path('login', views.login, name='login'),
+    path('', views.regist, name='regist'),
 ]
