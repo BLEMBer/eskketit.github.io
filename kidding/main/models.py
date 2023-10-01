2768e74f4893542f4866bb3515aed335fa052fb2
aqwua0 <mbmeetlater@gmail.com>
2023-10-01 16:53:03 +0300

    main

--- kidding/main/models.py
+++ kidding/main/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class shoes(models.Model):
-    title = models.CharField('Название пары' ,max_length=255)
+    title = models.CharField('Название пары', max_length=255)
+    content = models.TextField('Цена', blank=True)
+    content1 = models.TextField('Артикул', blank=6)
+
+class order(models.Model):
+    title1 = models.CharField('ФИО', max_length=100)
+    date = models.DateTimeField('Дата создания заказа', auto_now_add=True)
+    order = models.TextField('Состав заказа')
+
