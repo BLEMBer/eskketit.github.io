6d53d5001c61f9a20df761ff7cb6b4574e3b9fe1
aqwua0 <mbmeetlater@gmail.com>
2023-10-16 15:43:26 +0300

    main

--- kidding/main/views.py
+++ kidding/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'main/page2.html')
@@ -11,4 +12,15 @@ def about(request):
     return render(request, 'main/about_us.html')
 
 def regist(request):
+
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        password = request.POST['password']
+
+        myuser = User.objects.create_user(email, password, password)
+        myuser.save()
+
+        return redirect('main/login.html')
+
     return render(request, 'main/regist.html')
